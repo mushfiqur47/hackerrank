85d9e9a3344dcfec54dacfa80fe5537b8b0b8f2f
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-30 17:13:07 +0600

    Microsoft Visual C++ 6.0 SP6 resource compiler buffer overflow

---
+++ Microsoft Visual C++ - '.RC Resource Files' Local Buffer Overflow .cpp
@@ -0,0 +1,12 @@
+//*****************
+//
+//  Microsoft Visual C++ 6.0 SP6 resource compiler buffer overflow 
+//  vulnerability .rc resource files exploit 
+//
+//  vulnerability found / exploit built by porkythepig
+//
+//*****************
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "memory.h"
