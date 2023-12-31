4db5c06882c1e43abf0a4afaafcb1423006fe6d3
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-31 20:53:29 +0600

    1

--- Inherited Code.cpp
+++ Inherited Code.cpp
@@ -16,6 +16,20 @@ class BadLengthException {
         int what() {
             return n;
         }
-};
+}
 
+
+bool checkUsername(string username) {
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) {
+        throw BadLengthException(n);
+    }
+    for(int i = 0; i < n-1; i++) {
+        if(username[i] == 'w' && username[i+1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
+}
 
