7793de11ad0c37eecd185bd19f2f5c0f978878ae
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-31 20:56:54 +0600

    Inheritance Introduction

--- Inheritance Introduction.cpp
+++ Inheritance Introduction.cpp
@@ -16,3 +16,25 @@ class Triangle
             }
 };
 
+class Isosceles : public Triangle
+{
+        public:
+
+            void isosceles()
+                {
+                cout<<"I am an isosceles triangle\n";
+            }
+
+                void description()
+                {
+                        cout<<"In an isosceles triangle two sides are equal\n";
+                }
+};
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
+    return 0;
+}
