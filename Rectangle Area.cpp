d6b8bac627f6b47bac87718bed2db21a14a773d6
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-31 20:59:36 +0600

    2

--- Rectangle Area.cpp
+++ Rectangle Area.cpp
@@ -9,3 +9,13 @@ public:
     virtual void display() const {
         cout << width << ' ' << height << endl;
     }
+ protected:
+    int width;
+    int height;
+};
+
+class RectangleArea : public Rectangle {
+public:
+    void display() const override {
+        cout << (width * height) << endl;
+    }   
