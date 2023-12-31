7dc97cf013326fec4652e05ddabaf30e87d75c70
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-31 21:09:31 +0600

    4

--- Accessing Inherited Functions.cpp
+++ Accessing Inherited Functions.cpp
@@ -48,3 +48,27 @@ class B
         int getB(){
             return callB;
         }
+};
+
+class C
+{
+    public:
+        C(){
+            callC = 0;
+        }
+    private:
+        int callC;
+        void inc(){
+            callC++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 5;
+            inc();
+        }
+    public:
+        int getC(){
+            return callC;
+        }
+};        
