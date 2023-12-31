7084b08a670f0461c274f74c1b4be9e52994222e
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2023-12-31 21:08:44 +0600

    1

---
+++ Accessing Inherited Functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
